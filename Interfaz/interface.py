021754f667a02650510796cae6f35bf13950e46a
Agusbs98 <agusbuendia98@gmail.com>
2023-05-11 19:47:03 +0200

    sadfasdf

--- Interfaz/interface.py
+++ Interfaz/interface.py
@@ -2,21 +2,7 @@ from libs import *
 from predicts import procesar_archivo
 import gradio as gr
 
-## Crear la interfaz web
-#entrada = [
-#        gr.inputs.Dropdown(["XML","CSV"],default="XML",label= "Formato del archivo"),
-#        gr.inputs.Dropdown(["Hombre","Mujer"],default="Hombre",label= "Sexo"),
-#        #gr.inputs.Slider(label= "Edad",default=25),
-#        gr.inputs.Number(label= "Frecuencia (Hz)",default=500),
-#        gr.inputs.File(label="Selecciona un archivo.")
-#        ]
-#
-#salida = gr.outputs.Dataframe(label="Predicción automática.",type="pandas",headers = ['Red','Predicción'])
-#
-#Interface = gr.Interface(fn=procesar_archivo, inputs=entrada, outputs=salida, title="Analizador automático de ECG",flagging_dir="./outputs/")
-#Interface.launch(debug = True)
-
-with gr.Blocks() as demo:
+with gr.Blocks() as interface:
     with gr.Column():
         format = gr.inputs.Dropdown(["XML","CSV"],default="XML",label= "Formato del archivo")   
     with gr.Row():
@@ -27,7 +13,6 @@ with gr.Blocks() as demo:
         file = gr.inputs.File(label="Selecciona un archivo.")
         button = gr.Button(value='Analizar')
         out = gr.DataFrame(label="Diagnostico automático.",type="pandas",headers = ['Red','Predicción'])
-        #out = gr.outputs.Dataframe(label="Predicción automática.",type="pandas",headers = ['Red','Predicción'])
     button.click(fn=procesar_archivo,inputs=[format,number,unit,frec,file] ,outputs=out)
     
-demo.launch()
+interface.launch()
